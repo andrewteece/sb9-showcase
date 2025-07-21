2f3bfc301934a0e29be24c2c5e8ecb0b8baac5ae
bartstc <bartst010395@gmail.com>
2025-07-21 13:14:14 +0000

    chore: add utility types

--- src/features/products/types/IProduct.ts
+++ src/features/products/types/IProduct.ts
@@ -1,8 +1,12 @@
+import type { Branded } from "@/types/Branded";
+
 import { Category } from "./Category";
 import type { IRating } from "./IRating";
 
+export type ProductId = Branded<number, "ProductId">;
+
 export interface IProduct {
-  id: number;
+  id: ProductId;
   title: string;
   description: string;
   category: Category;
